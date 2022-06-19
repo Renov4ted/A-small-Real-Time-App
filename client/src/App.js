fbdf868d4b455f810601cec0b4d29b6e4ef20152
Fatih Unal <fatihsultan069@gmail.com>
2022-06-19 13:52:13 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import Palette from './components/Palatte';
+import './App.css';
+import {init, subscribe } from "../src/socketApi"
+import {useEffect, useState} from 'react';
+
+function App() {
+  const [activeColor, setActiveColor] = useState("282c34")
+
+  useEffect(()=>{
+    init()
+
+    subscribe((color)=> {
+      setActiveColor(color)
+    })
+  },[])
+
+  return (
+    <div className="App" style={{backgroundColor : activeColor}}>
+      <Palette activeColor={activeColor}/>
+    </div>
+  );
+}
+
+export default App;
